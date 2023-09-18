b88c4d410750a6d2c2605d9c241bd76b1d04aaea
Dhruven Prajapati <138546545+DhruvenPrajapati@users.noreply.github.com>
2023-09-17 20:39:35 -0500

    Add files via upload

---
+++ Data_visualization_query_pat_2.sql
@@ -0,0 +1,36 @@
+/* Queries used for data visualization in Tableau public and store in excel sheet*/
+
+--1 total case, deaths and %
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From CovidDataAnalysis..covidDeaths
+where continent is not null 
+order by 1,2
+
+
+-- 2. 
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From CovidDataAnalysis..covidDeaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International', 'High income', 'Upper middle income','Low income', 'Oceania')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3.
+
+Select location, population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDataAnalysis..covidDeaths
+Group by location, population
+order by PercentPopulationInfected desc
+
+
+-- 4.
+
+Select location, population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDataAnalysis..covidDeaths
+Group by location, population, date
+order by PercentPopulationInfected desc
